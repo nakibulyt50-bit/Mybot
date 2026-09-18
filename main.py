5b685ccbc7163202ec6090ddd900275812e58e6f
nakibulyt50-bit <nakibulyt50@gmail.com>
2026-09-18 13:26:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 from twocaptcha import TwoCaptcha
 
 # --- CONFIGURATION ---
@@ -46,9 +45,14 @@ def send_voice_to_telegram(audio_path):
             print(f"Failed to send voice: {response.text}")
 
 def main():
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--headless") # Background-e run korate chaile comments hataien
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.binary_location = "/usr/bin/chromium"
+
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
 
     try:
         # 1. Login Page Open
